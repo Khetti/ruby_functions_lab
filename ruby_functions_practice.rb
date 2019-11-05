f37c91d52d426ae92abae9fdbdfc82e3b132bdc4
Greg Shanks <greg.shanks@gmail.com>
2019-11-05 15:49:52 +0000

    First Commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,35 @@
+# Homework for day 2, write the functions for testing here.
+def return_10
+  return 10
+end
+
+def add(first_number, second_number)
+  return first_number + second_number
+end
+
+def subtract(first_number, second_number)
+  return first_number - second_number
+end
+
+def multiply(first_number, second_number)
+  return first_number * second_number
+end
+
+def divide(first_number, second_number)
+  return first_number / second_number
+end
+
+def length_of_string
+end
+
+def join_string
+end
+
+def add_string_as_number
+end
+
+def number_to_full_name
+end
+
+def number_to_short_month_name
+end
